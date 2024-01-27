05d5f81a9163928206c07870a5213a99267a5b2e
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2024-01-26 23:45:02 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom-styles.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Rubik:wght@600&display=swap" rel="stylesheet">
+
+    <title>Todo-list</title>
+</head>
+
+<body class="body-style">
+
+    <nav class="menu-style">
+        <div class="menu-text">
+            MENU
+        </div>
+
+        <div class="task-style">
+            TAREFAS
+            <button class="button-style">
+                Por vir
+            </button>
+
+            <button class="button-style">
+                Hoje
+            </button>
+
+            <button class="button-style">
+                Calendário
+            </button>
+
+            <button class="button-style">
+                Notas adesivas
+            </button>
+        </div>
+    </nav>
+
+    <main class="main-style">
+        <div class="today-style">
+            Hoje
+        </div>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
